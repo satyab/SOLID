6547c03240125fe84496745297546bf452bf26ef
satyajit <satyajit@clevertap.com>
2020-05-04 01:35:47 +0530

    Add problem example for SRP

---
+++ src/SRP/problem/Demo.java
@@ -0,0 +1,12 @@
+package SRP.problem;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Journal j = new Journal();
+        j.add("I am writing code");
+        j.add("I am having lunch");
+
+        System.out.println(j);
+    }
+}
---
+++ src/SRP/problem/Journal.java
@@ -0,0 +1,28 @@
+package SRP.problem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Journal {
+
+    private List<String> entries;
+    private int count;
+
+    public Journal(){
+        this.entries = new ArrayList<>();
+        this.count = 0;
+    }
+
+    public void add(String text) {
+        this.entries.add("" + (++count) + ": " + text);
+    }
+
+    public void remove(int index){
+        this.entries.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(System.lineSeparator(), this.entries);
+    }
+}
